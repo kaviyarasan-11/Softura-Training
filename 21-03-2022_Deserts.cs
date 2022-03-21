5fec8337d42140a5d3195791b04e5eab483010c7
kaviyarasan-11 <84147211+kaviyarasan-11@users.noreply.github.com>
2022-03-21 17:09:44 +0530

    Add files via upload

---
+++ 21-03-2022_Deserts.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+namespace ConsoleApp5
+{
+    class Deserts
+    {
+        public static void Main()
+        {
+           
+            Queue Desserts = new Queue();
+            Desserts.Enqueue("1.Gajar ka halwa");
+            Desserts.Enqueue("2.Kaju katli");
+            Desserts.Enqueue("3.Motichoor laddu");
+            Desserts.Enqueue("4.Rasgulla");
+            Desserts.Enqueue("5.Sohan halwa");
+            Desserts.Enqueue("6.Rabri");
+            Desserts.Enqueue("7.Kuzhi paniyaram");
+            Console.WriteLine("List of Desserts :"+ Desserts.Count);
+            /*while(Desserts.Count>0)
+            {
+                Console.WriteLine( Desserts.Dequeue());
+            }*/  
+            
+            FileStream fs = new FileStream("D:\\Softura tasks\\softura Task10\\21-03-2022_Collections of Desserts.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryWriter  bw = new BinaryWriter(fs);
+            Console.WriteLine("List of Desserts :" + Desserts.Count);
+            Console.WriteLine("Desserts of Restaurant :" );
+            foreach (string coll in Desserts)
+            {
+                bw.Write("\n"+coll);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+
+            FileStream fs1 = new FileStream("D:\\Softura tasks\\softura Task10\\21-03-2022_Collections of Desserts.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs1);
+            string str = br.ReadString();
+            br.Close();
+            fs1.Close();
+
+            FileInfo fi = new FileInfo(@"D:\\Softura tasks\\softura Task10\\21-03-2022_Collections of Desserts.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 21-03-2022_Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+namespace ConsoleApp5
+{
+    class Employee
+    {
+        public static void Main()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(1,"Name:Rukesh,ID:1234,Gender:Male,Age:21");
+            dic.Add(2, "Name:Nithin,ID:1235,Gender:Male,Age:21");
+            dic.Add(3, "Name:Kavi,ID:1236,Gender:Male,Age:21");
+            FileStream fs = new FileStream("D:\\Softura tasks\\softura Task10\\21-03-2022_Employee.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            foreach (var v in dic)
+                Console.WriteLine("Employee Id: {0}  {1}", v.Key, v.Value);
+            foreach (var v in dic)
+            {
+                bw.Write(v.Key + v.Value);
+                bw.Write("\n");
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            Console.ReadLine();
+        }
+    }
+}
