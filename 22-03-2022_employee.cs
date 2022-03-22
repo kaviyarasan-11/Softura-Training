52650b3a0ab00d7bd870a96398bb702831e8e6fd
kaviyarasan-11 <84147211+kaviyarasan-11@users.noreply.github.com>
2022-03-22 16:22:09 +0530

    Add files via upload

---
+++ 22-03-2022_employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+namespace ConsoleApp2
+{
+    [Serializable]
+    class Employee
+    {
+        public string empid = "1234";
+        public string name = "Nithin";
+        public int age = 21;
+        public string gender = "Male";
+        
+    }
+    class Empl
+    {
+        public void SerialtoFile()
+        {
+            Employee obj1 = new Employee();
+            FileStream fs = new FileStream("emp.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, obj1);
+            fs.Close();
+        }
+        public void DeSerialData()
+        {
+            FileStream fs = new FileStream("emp.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            Employee obj2 = (Employee)bf.Deserialize(fs);
+            Console.WriteLine(obj2.empid);
+            Console.WriteLine(obj2.name);
+            Console.WriteLine(obj2.age);
+            Console.WriteLine(obj2.gender);
+            
+        }
+
+        public static void Main()
+        {
+            Empl obj = new Empl();
+            obj.SerialtoFile();
+            obj.DeSerialData();
+            Console.ReadLine();
+
+        }
+    }
+}
+   
+    
+
