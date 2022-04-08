0432ce43f2e7e30bed39404b00e7f8ad2c40f340
kaviyarasan-11 <84147211+kaviyarasan-11@users.noreply.github.com>
2022-04-09 00:49:38 +0530

    Add files via upload

---
+++ 08-04-2022-Task25.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day25
+{
+    class _08_04_2022_Task25
+    {
+        public  static int CGoat(int eyes,int legs)
+        {
+            int count = 0;
+            count = (legs)-(eyes-1) ;
+            count = count /2 ;
+            return count;
+        }
+        public static void Main()
+        {
+            int eyes = 52, legs = 80;
+            int Goat = CGoat(eyes,legs);
+            Console.WriteLine("No of Goat:"+" "+Goat);
+            Console.WriteLine("No of Duck:" + " " + (Goat-2));
+        }
+    }
+}
